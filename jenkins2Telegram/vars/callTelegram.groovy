9a05f902f1a589fee788d9bc7d3730375e14ecff
Alexander Juhn <home@juhn.net>
2022-04-08 22:19:40 +0200

    HI-33 Added Telegram call

--- jenkins2Telegram/vars/callTelegram.groovy
+++ jenkins2Telegram/vars/callTelegram.groovy
@@ -1,5 +1,8 @@
 #!/usr/bin/env groovy
 
-def call(String name){
-    echo "Hello World from ${name}!"
+def call(String botToken, String chatId, String status, String job, String build){
+    curl -X POST \
+     -H 'Content-Type: application/json' \
+     -d '{"chat_id": "'${CHAT_ID}'","parse_mode":"HTML", "text": "<b>[Jenkins - '${STATUS}']</b>\nJob: '${JOB}'\nBuild: '${BUILD}'", "disable_notification": true}' \
+     https://api.telegram.org/bot$BOT_TOKEN/sendMessage
 }
