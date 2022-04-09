95918a54b84c2be0f518832ea5fa5cd5fa57da8d
Alexander Juhn <home@juhn.net>
2022-04-09 14:10:45 +0200

    HI-33 Fixed TelegramCall

--- jenkins2Telegram/vars/callTelegram.groovy
+++ jenkins2Telegram/vars/callTelegram.groovy
@@ -1,8 +1,7 @@
 #!/usr/bin/env groovy
 
 def call(String botToken, String chatId, String status, String job, String build){
-    curl -X POST \
-     -H 'Content-Type: application/json' \
-     -d '{"chat_id": "'${CHAT_ID}'","parse_mode":"HTML", "text": "<b>[Jenkins - '${STATUS}']</b>\nJob: '${JOB}'\nBuild: '${BUILD}'", "disable_notification": true}' \
-     https://api.telegram.org/bot$BOT_TOKEN/sendMessage
+    sh """
+        curl -X POST -H 'Content-Type: application/json' -d '{"chat_id": "'${chatId}'","parse_mode":"HTML", "text": "<b>[Jenkins - '${status}']</b>\nJob: '${job}'\nBuild: '${build}'", "disable_notification": true}'      https://api.telegram.org/bot$botToken/sendMessage
+    """
 }
