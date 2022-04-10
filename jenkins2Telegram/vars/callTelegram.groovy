75415a62bd9404fbd13743e84d4c455b05e5fd5c
Alexander Juhn <home@juhn.net>
2022-04-10 21:44:05 +0200

    HI-33 Added credentials access

--- jenkins2Telegram/vars/callTelegram.groovy
+++ jenkins2Telegram/vars/callTelegram.groovy
@@ -1,7 +1,8 @@
 #!/usr/bin/env groovy
-
-def call(String botToken, String chatId, String status, String job, String build){
-    sh """
-        curl -X POST -H 'Content-Type: application/json' -d '{"chat_id": "'${chatId}'","parse_mode":"HTML", "text": "<b>[Jenkins - '${status}']</b>\nJob: '${job}'\nBuild: '${build}'", "disable_notification": true}'      https://api.telegram.org/bot$botToken/sendMessage
-    """
+def call(String status, String job, String build){
+    withCredentials([string(credentialsId: 'TelegramChatID', variable: 'CHAT_ID'), string(credentialsId: 'TelegramBotToken', variable: 'BOT_TOKEN')]) {
+        sh """
+            curl -X POST -H 'Content-Type: application/json' -d '{"chat_id": "'${CHAT_ID}'","parse_mode":"HTML", "text": "<b>[Jenkins - '${status}']</b>\nJob: '${job}'\nBuild: '${build}'", "disable_notification": true}'      https://api.telegram.org/bot${BOT_TOKEN}/sendMessage
+        """
+    }
 }
